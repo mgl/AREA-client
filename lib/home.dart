b609ce15a032400098f27dfa9a41dff966f3f7bc
MT <mael.teyssedre@epitech.eu>
2022-02-28 06:52:52 +0100

    refactoring home view

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'package:client/layout/adaptive.dart';
+import 'package:client/layout/text_scale.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,23 +11,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-  void _incrementCounter() => setState(() => _counter++);
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDesktop = isDisplayDesktop(context);
+    const drawerHeader = UserAccountsDrawerHeader(
+        accountName: Text('name'),
+        accountEmail: Text('email@test.com'),
+        currentAccountPicture: CircleAvatar(
+            backgroundImage: AssetImage('assets/epilogo.png'),
+            backgroundColor: Colors.black87));
+    final drawerItems = ListView(children: [
+      drawerHeader,
+      ListTile(
+          title: const Text('data'),
+          leading: const Icon(Icons.comment),
+          onTap: () {
+            Navigator.pop(context);
+          }),
+      ListTile(
+          title: const Text('data'),
+          leading: const Icon(Icons.comment),
+          onTap: () {
+            Navigator.pop(context);
+          })
+    ]);
     return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              Text('$_counter', style: Theme.of(context).textTheme.headline4)
-            ])),
-        floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add)));
+        appBar: AppBar(title: const Text('AREA PROJECT')),
+        body: const Center(
+            child: Padding(
+                padding: EdgeInsets.all(50.0),
+                child: Text('Work In Progress'))),
+        drawer: Drawer(child: drawerItems));
   }
 }
