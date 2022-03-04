e3416ebf1a4c88b3073d433c42d3e1f530082135
Romain-GABET <62942880+Romain-GABET@users.noreply.github.com>
2022-03-04 22:05:20 +0100

    display real value

--- lib/views/home/twitter_button.dart
+++ lib/views/home/twitter_button.dart
@@ -11,33 +11,33 @@ class _TwitterButtonState extends State<TwitterButton> {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-          onPressed: () {
-            setState(() {
-              _connectedToTwitter = (!_connectedToTwitter) ? true : false;
-            });
-          },
-          style: TextButton.styleFrom(
-              backgroundColor: Colors.grey[200],
-              primary: Colors.black,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12))),
-          child: Row(children: [
-            Container(
-              height: 30,
-              width: 30,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/twitter-logo.jpg'),
-                      fit: BoxFit.cover),
-                  shape: BoxShape.circle),
-            ),
-            const SizedBox(width: 20),
-            (!_connectedToTwitter)
-                ? const Text('Connect to Twitter')
-                : const Text(
-                    'Connected to Twitter',
-                    style: TextStyle(color: Colors.green),
-                  ),
-          ]));
+        onPressed: () {
+          setState(() {
+            _connectedToTwitter = (!_connectedToTwitter) ? true : false;
+          });
+        },
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.grey[200],
+            primary: Colors.black,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12))),
+        child: Row(children: [
+          Container(
+            height: 30,
+            width: 30,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/twitter-logo.jpg'),
+                    fit: BoxFit.cover),
+                shape: BoxShape.circle),
+          ),
+          const SizedBox(width: 20),
+          (!_connectedToTwitter)
+              ? const Text('Connect to Twitter')
+              : const Text(
+                  'Connected to Twitter',
+                  style: TextStyle(color: Colors.green),
+                ),
+        ]));
   }
 }
